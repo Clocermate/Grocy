9ff42f1e4ae85d4e459996e6e006780e3d948da7
Clocermate <clocermate@gmail.com>
2023-05-17 21:02:53 +0530

    login button done

--- script.js
+++ script.js
@@ -6,3 +6,7 @@ let ShoppingCart=document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick=()=>{
     ShoppingCart.classList.toggle('active');
 }
+let Login=document.querySelector('.login-form');
+document.querySelector('#login-btn').onclick=()=>{
+    Login.classList.toggle('active');
+}
