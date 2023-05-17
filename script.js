63c21738bf1ace43ef51ad79c3863ea15edc11db
Clocermate <clocermate@gmail.com>
2023-05-17 21:30:54 +0530

    added js for menu button & added media queries

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 let searchForm=document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick=()=>{
     searchForm.classList.toggle('active');
+    Menu.classList.remove('active');
+    ShoppingCart.classList.remove('active');
+    Login.classList.remove('active');
 }
 let ShoppingCart=document.querySelector('.shopping-cart');
 document.querySelector('#cart-btn').onclick=()=>{
     ShoppingCart.classList.toggle('active');
+    Menu.classList.remove('active');
+    searchForm.classList.remove('active');
+    Login.classList.remove('active');
 }
 let Login=document.querySelector('.login-form');
 document.querySelector('#login-btn').onclick=()=>{
     Login.classList.toggle('active');
+    Menu.classList.remove('active');
+    searchForm.classList.remove('active');
+    ShoppingCart.classList.remove('active');
+}
+let Menu=document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick=()=>{
+    Menu.classList.toggle('active');
+    searchForm.classList.remove('active');
+    ShoppingCart.classList.remove('active');
+    Login.classList.remove('active');
+}
+window.onscroll=()=>{
+    Menu.classList.remove('active');
+    searchForm.classList.remove('active');
+    ShoppingCart.classList.remove('active');
+    Login.classList.remove('active');
 }
