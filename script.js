ab70f464381ab6c9f6be0c5dfdcdcb39c1e9127f
Clocermate <clocermate@gmail.com>
2023-05-17 17:07:03 +0530

    header part completed

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let searchForm=document.querySelector('.search-form');
 document.querySelector('#search-btn').onclick=()=>{
     searchForm.classList.toggle('active');
 }
+let ShoppingCart=document.querySelector('.shopping-cart');
+document.querySelector('#cart-btn').onclick=()=>{
+    ShoppingCart.classList.toggle('active');
+}
