3660237609f5a437c196896179a1718e16e9ccb6
Clocermate <clocermate@gmail.com>
2023-05-18 19:02:55 +0530

    added swiper js

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ window.onscroll=()=>{
     ShoppingCart.classList.remove('active');
     Login.classList.remove('active');
 }
+
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay:{
+        delay:7500,
+        disableOnInteraction:false,
+    },
+    breakpoints:{
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1020: {
+        slidesPerView: 5,
+      },
+    },
+  });
